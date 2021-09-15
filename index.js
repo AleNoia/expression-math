14e5204ef1980ce930979b225b453a19de459dd1
IgorNoia <igornoiasilva@gmail.com>
2021-09-15 19:10:58 -0300

    Improving calculation logic

--- index.js
+++ index.js
@@ -7,38 +7,63 @@ const rl = readline.createInterface({
 });
 
 let expression = String;
-let resultExpression = Number;
+
+// Regex tests
+let parens = /\(([0-9+\-*/\^ .]+)\)/; // Identifying if have parenthetical
+let sub = /(\d+(?:\.\d+)?) ?- ?(\d+(?:\.\d+)?)/; // Identifying if have subtraction
+let div = /(\d+(?:\.\d+)?) ?\/ ?(\d+(?:\.\d+)?)/; // Identifying if have division
+let add = /(\d+(?:\.\d+)?) ?\+ ?(\d+(?:\.\d+)?)/; // Identifying if have addition
+let mul = /(\d+(?:\.\d+)?) ?\* ?(\d+(?:\.\d+)?)/; // Identifying if have multiplication
+let exp = /(\d+(?:\.\d+)?) ?\^ ?(\d+(?:\.\d+)?)/; // Identifying if have exponentials
 
 // Functions
-// Verift if have a division by zero
-function divisionByZero() {
-  if (resultExpression === Infinity) {
-    console.error("You cannot do divisions by zero");
-  }
-}
-
-// Verifying the expression and the character
-function verifyCharacter() {
-  if (expression.length === 0) {
-    console.error("Insert the expression");
-  } else if (expression.includes("^")) {
-    expression = expression.replace("^", "**");
-  } else if (expression.includes(":")) {
-    expression = expression.replace(":", "/");
-  }
-  return expression;
-}
 
 // Calculating the expression sent by the user
-function calculating() {
-  resultExpression = eval(expression);
-  return console.log("The result is ", resultExpression);
+function calculating(expression) {
+  if (isNaN(Number(expression))) {
+    if (parens.test(expression)) {
+      let newExpr = expression.replace(parens, function (match, subExpr) {
+        return calculating(subExpr);
+      });
+      return calculating(newExpr);
+    } else if (exp.test(expression)) {
+      let newExpr = expression.replace(exp, function (match, base, pow) {
+        return Math.pow(Number(base), Number(pow));
+      });
+      return calculating(newExpr);
+    } else if (mul.test(expression)) {
+      let newExpr = expression.replace(mul, function (match, a, b) {
+        return Number(a) * Number(b);
+      });
+      return calculating(newExpr);
+    } else if (div.test(expression)) {
+      let newExpr = expression.replace(div, function (match, a, b) {
+        if (b != 0) return Number(a) / Number(b);
+        else throw new Error("Division by zero");
+      });
+      return calculating(newExpr);
+    } else if (add.test(expression)) {
+      let newExpr = expression.replace(add, function (match, a, b) {
+        return Number(a) + Number(b);
+      });
+      return calculating(newExpr);
+    } else if (sub.test(expression)) {
+      let newExpr = expression.replace(sub, function (match, a, b) {
+        return Number(a) - Number(b);
+      });
+      return calculating(newExpr);
+    } else {
+      return "Dont is a expression";
+    }
+  }
+  return Number(expression);
 }
 
 // Verifying if the user want to close or not the application
 function continueOrNotAplication() {
   rl.question("Do you want to continue? [y/n] ", function (value) {
     if (value == "y") {
+      console.log(" ");
       start();
     } else {
       rl.close();
@@ -51,12 +76,12 @@ function start() {
   rl.question("Insert the expression math: ", function (value) {
     try {
       expression = value;
-      verifyCharacter();
-      divisionByZero();
-      calculating();
+      console.log("The result of the expression is ", calculating(expression));
+      console.log(" ");
       continueOrNotAplication();
     } catch (err) {
-      console.error("An error occurred in the application: ", err);
+      console.log("Same error happened: ", err);
+      continueOrNotAplication();
     }
   });
 }
